3ab7d93a4d9c2c5360c04215bffbf64acbb987a4
Piotr Kedra <piotr.k9750@gmail.com>
2021-11-14 12:41:22 +0100

    init taks 2

---
+++ task2/server.py
@@ -0,0 +1,5 @@
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
