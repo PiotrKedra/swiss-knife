f5d3bf9a863a8ff0662c517c44d8d6ef925d552f
Piotr Kedra <piotr.k9750@gmail.com>
2021-11-14 13:00:06 +0100

    basic get response

--- task2/server.py
+++ task2/server.py
@@ -1,5 +1,22 @@
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+import http.server 
+import socketserver
+
+
+class CustomHandler(http.server.BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        print("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("Hello world".format(self.path).encode('utf-8'))
+
+
+def run(server_class=http.server.HTTPServer, handler_class=CustomHandler):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
+run()
