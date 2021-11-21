57201ea7cd5701b42765a480f7763d536fd72a8d
Mark Pirkowski <ge93wut@mytum.de>
2021-11-21 14:25:01 +0100

    Add folder for benchmark results

--- task2/reproduce.py
+++ task2/reproduce.py
@@ -1,5 +1,4 @@
 # !/usr/bin/env python3
-
 import sys
 
 if sys.version_info < (3, 0, 0):
@@ -7,12 +6,10 @@ if sys.version_info < (3, 0, 0):
     sys.exit(1)
 
 import os
-import shutil
-from typing import IO, Any, Callable, List
+from typing import IO, Any, Callable
 from pathlib import Path
 
 ROOT = Path(__file__).parent.resolve()
-APPS_PATH = ROOT.joinpath("apps", "nix")
 HAS_TTY = sys.stderr.isatty()
 NUMBER_CLIENTS = [1, 2, 4, 8, 16, 32]
 
@@ -38,37 +35,38 @@ def setup() -> None:
     """
 
     # pull benchmark tool
-    os.system("docker pull willamyeih/wrk")
+    os.system("docker pull williamyeh/wrk")
     # build image for servers
-    os.system("docker build -t server_teamd - < server.Dockerfile")
+    os.system("docker build -t server_teamd -f server.Dockerfile .")
     # build image for plots
-    os.system("docker build -t plot_results_teamd - < plot.Dockerfile")
+    os.system("docker build -t plot_results_teamd -f plot.Dockerfile .")
 
 
 def evaluate(num_con, duration, port) -> None:
-    # run benchmarks
+    # run benchmarks and output the results into folder ./wrk_results
     for i in NUMBER_CLIENTS:
         os.system(
-            f"docker --rm -it --net=host willamyeih/wrk -t{i} -c{num_con} -d{duration}s http://192.168.55.1:{port}"
+            f"docker run --rm -it --net=host williamyeh/wrk -t{i} -c{num_con} -d{duration}s http://192.168.55.1:{port} "
+            f"> wrk_results/clients_nr_{i}.txt"
         )
 
 
 def generate_graphs() -> None:
+    results = ROOT.joinpath("wrk_results")
+    results.mkdir()
+
     # stop possible running container with server of team D
     os.system('docker stop server_teamd')
     # start container for basic task
-    os.system(f'docker run --rm -it --net=host -v "$(pwd)/basic":/scripts server_teamd')
+    os.system(f'docker run --rm -itd --net=host -v "$(pwd)/basic":/scripts server_teamd')
     evaluate(num_con=400, duration=10, port=800)
     os.system('docker stop server_teamd')
 
 
+
+
+
 def main() -> None:
-    sudo = shutil.which("sudo", mode=os.F_OK | os.X_OK)
-    if sudo is None:
-        warn(
-            "For running the server the sudo command is needed!"
-        )
-        sys.exit(1)
     setup()
 
 
