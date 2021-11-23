e35b89d2ce21d617dba72f95869ab1d99b1af7ff
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2021-11-23 20:29:09 +0100

    Basic Server close connection

--- task2/basic/server.py
+++ task2/basic/server.py
@@ -15,12 +15,12 @@ def main():
     http_server.setsockopt(socket.SOL_SOCKET, 25, str(network_settings['interface'] + '\0').encode('utf-8'))
     http_server.bind(('', network_settings['port']))
     http_server.listen(128)
-    while True:
-        connection, address = http_server.accept()
-        data = connection.recv(1024)
-        data = b"HTTP/1.1 200 OK\r\n\r\n" + data
-        print(data)
-        connection.sendall(data)
+    connection, address = http_server.accept()
+    data = connection.recv(1024)
+    data = b"HTTP/1.1 200 OK\r\n\r\n" + data
+    print(data)
+    connection.sendall(data)
+    connection.close()
 
 
 if __name__ == '__main__':
