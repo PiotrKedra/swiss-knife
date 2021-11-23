a73af28ae50dbb23c776be16eb4557f710013480
Piotr Kedra <piotr.k9750@gmail.com>
2021-11-23 23:16:13 +0100

    io_uring v2

--- task2/io_uring/server.py
+++ task2/io_uring/server.py
@@ -1,71 +1,109 @@
 import socket
-import re
-import os
+import time
 from liburing import *
-import resource
-
-# increase max open connections (while benchmarking with wrk we exceeded limit)
-resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
+import errno
 
 
-def service_client(new_socket: object, request: str):
+EVENT_TYPE_ACCEPT = 0
+EVENT_TYPE_READ = 1
+EVENT_TYPE_WRITE = 2
 
+def add_accept_request(ring, server_socket, client_addr, client_addr_len):
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_accept(sqe, server_socket, client_addr, client_addr_len, 0)
+    io_uring_sqe_set_data(sqe, EVENT_TYPE_ACCEPT)
+    io_uring_submit(ring)
+
+def add_read_request(ring, client_socket):
+    buffer = bytearray(1024)
+    iov = iovec(buffer)
+
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_readv(sqe, client_socket, iov[0].iov_base, iov[0].iov_len, 0)
+    io_uring_sqe_set_data(sqe, EVENT_TYPE_READ)
+    io_uring_submit(ring)
+
+def add_write_request(ring, client_socket):
+    sqe = io_uring_get_sqe(ring) # sqe(submission queue entry)
     response_body = "<h1>hello world</h1>"
     resposne = "HTTP/1.1 200 OK\r\n"
     resposne += "Content-Length:%d\r\n" % len(response_body)
     resposne += "\r\n"
-    resposne += response_body
-    new_socket.send(resposne.encode("utf-8"))
+    response = resposne.encode("utf-8")
+    buffer = bytearray(response)
+    iov = iovec(buffer)
+
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_writev(sqe, client_socket, iov[0].iov_base, iov[0].iov_len, 0)
+    io_uring_sqe_set_data(sqe, EVENT_TYPE_WRITE)
+    io_uring_submit(ring)
+
+def add_close_request(ring, client_socket):
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_close(sqe, client_socket)
+    io_uring_submit(ring)
 
 
-def main():
-    http_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    http_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    http_server.setsockopt(socket.SOL_SOCKET, 25, str("swissknife0" + '\0').encode('utf-8'))
-    http_server.bind(("192.168.55.1", 800))
-    http_server.listen(128)
-    http_server.setblocking(False)
+ring = io_uring()
+cqes = io_uring_cqes()
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, True)
+sock.bind(("", 8000))
+sock.listen(128)
+sockfd = sock.fileno()
 
-    # epl = select.epoll()
-    ring = io_uring()
+try:
+    
+    io_uring_queue_init(256, ring, 0)
     cqes = io_uring_cqes()
-    
-    io_uring_queue_init(8, ring, 0)
-    
-    # Register the FD (file descriptor) corresponding to the listening socket into epoll
-    #epl.register(http_server.fileno(), select.EPOLLIN)
+    addr, addrlen = sockaddr()
 
-    # Storing the correspondence between fd file descriptors and sockets
-    fd_event_dict: dict = {}
-    while True:
+    add_accept_request(ring, sockfd, addr, addrlen)
 
-        client, info = http_server.accept()
-        fd = open(ring, cqes, '/tmp/liburing-test-file.txt', os.O_CREAT | os.O_RDWR)
-        sqe = io_uring_get_sqe(ring)  # sqe(submission queue entry)
-        io_uring_prep_openat(sqe, -1, client.fileno(), os.O_CREAT | os.O_RDWR, 0o660)
+    client_sockets = []
+    close_sockets = []
+
+    while(True):
+
+        print('Loop iteration...')
+        io_uring_wait_cqe(ring, cqes)
+
+        # get current request from the queue
+        cqe = cqes[0]
+
+        # handle io_uring errors
+        if(cqe.res < 0):
+            print('# Error (' + str(cqe.res) + ') ' + errno.errorcode[abs(cqe.res)] + ' in event_type: ' + str(cqe.user_data))
+            print('Exiting...')
+            exit()
+        else:
+            print('No error. Code ' + str(cqe.res) + '. While event_type: ' + str(cqe.user_data))
+
         
-        # By default, the blockage will be blocked until the os detects the arrival of data and informs the program by event notification, then the blockage will be removed.
-        #fd_event_list: list = epl.poll()  # [(socket corresponding file descriptor, what event is this file descriptor),...]
-        # for fd, event in fd_event_list:
-        #     # If the listener socket has data coming, it waits for a new client connection.
-        #     if fd == http_server.fileno():
-        #         client, info = http_server.accept()
-        #         # Register the new socket into epoll
-        #         epl.register(client.fileno(), select.EPOLLIN)
-        #         # Store the corresponding relationship between file descriptors and sockets in a dictionary
-        #         fd_event_dict[client.fileno()] = client
-        #     elif event == select.EPOLLIN:
-        #         # Determine whether a connected socket has data sent in
-        #         recv_data: str = fd_event_dict[fd].recv(1024).decode("utf-8")
-        #         if recv_data:
-        #             service_client(fd_event_dict[fd], recv_data)
-        #         else:
-        #             print("### if no data close connection and removed from epoll FD")
-        #             fd_event_dict[fd].close()
-        #             epl.unregister(fd)
-        #             del fd_event_dict[fd]
+        if(cqe.user_data == EVENT_TYPE_ACCEPT):
+            add_accept_request(ring, sockfd, addr, addrlen)
+            # here as cqe.res we should get client_socket as a result from accept request?
+            add_write_request(ring, cqe.res)
+            close_sockets.append(cqe.res)
+
+        # elif(cqe.user_data == EVENT_TYPE_READ):
+        #     client_socket = client_sockets.pop(0)
+        #     add_write_request(ring, client_socket)
+        #     close_sockets.append(client_socket)
+
+        elif(cqe.user_data == EVENT_TYPE_WRITE):
+            client_socket = close_sockets.pop(0)
+            add_close_request(ring, client_socket)
         
 
-if __name__ == '__main__':
-    main()
+        # clear current request & move to next
+        io_uring_cqe_seen(ring, cqe)
+
+        # sleep to see output
+        time.sleep(2)
+
+finally:
+    sock.close()
+    io_uring_queue_exit(ring)
