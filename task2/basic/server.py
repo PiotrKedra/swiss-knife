760464a6fcd55706625d90144446968abe4f0796
Mark Pirkowski <ge93wut@mytum.de>
2021-11-23 20:45:25 +0100

    Modify basic server and remove redundant files

--- task2/basic/server.py
+++ task2/basic/server.py
@@ -18,10 +18,12 @@ def main():
     while True:
         connection, address = http_server.accept()
         data = connection.recv(1024)
-        data = b"HTTP/1.1 200 OK\r\n\r\n" + data
-        print(data)
-        connection.sendall(data)
+        if data:
+            data = b"HTTP/1.1 200 OK\r\n\r\n" + data
+            print(data)
+            connection.sendall(data)
         connection.close()
+
 
 if __name__ == '__main__':
     main()
