102b88292952dbe76fcd74e5ad41917b9936f2b8
Mark Pirkowski <ge93wut@mytum.de>
2021-11-24 13:33:27 +0100

    Try ipv6 instead of ipv4

--- task2/basic/server.py
+++ task2/basic/server.py
@@ -16,10 +16,12 @@ def main():
     )
     (family, sock_type, proto, canon_name, sock_address) = address_info[0]
     http_server = socket.socket(family, sock_type, proto)
+    http_server.connect(())
 
     # http_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # http_server.setsockopt(socket.SOL_SOCKET, 25, str(network_settings['interface'] + '\0').encode('utf-8'))
-    # http_server.bind(('', network_settings['port']))
+
+    http_server.bind(('', network_settings['port']))
     http_server.listen(128)
     while True:
         connection, address = http_server.accept()
