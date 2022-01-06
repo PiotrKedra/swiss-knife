b90449ff94fa73efa7f63cf32e0f47b3933f4d47
piotrkedra <piotr.k9750@gmail.com>
2022-01-06 15:40:35 +0100

    parsec

---
+++ task3/multithread/parsec/plot.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+
+def read_from_file(file_name):
+    f = open(file_name,)
+    core_numbers = []
+    calculation_times = []
+    for line in f:
+        
+        stripped_line = line.strip()
+
+        if (stripped_line[:4] == 'real'):
+            min = ''
+            sec = ''
+            if_min = True
+            for char in stripped_line[5:]:
+                if (if_min == True):
+                    if (char != 'm'):
+                        min += char
+                    else:
+                        if_min = False
+                        continue
+                else:
+                    if (char != 's'):
+                        sec += char
+                    else:
+                        return float(sec) + (float(min) * 60)
+
+        
+CORES = ['1','2','4','8','16','32','64','128']
+
+def get_data_for_app(app_name, data_type):
+    data = []
+    for core in CORES:
+        data.append(read_from_file('results/' + app_name + '_' + data_type + '_' + core + '.txt'))
+
+    return data
+
+
+def generate_plot(result_name):
+    fig, axes = plt.subplots(1, 3)
+    fig.suptitle(result_name +  ' execution time by increasing number of threads for different data size.')
+    fig.set_size_inches(15, 4.5, forward=True)
+
+
+    data_sizes = ["small", "medium", "large"]
+    
+    axes_num = 0
+    for data_size in data_sizes:
+
+        y = get_data_for_app(result_name, data_size)
+        x = CORES
+
+        axes[axes_num].bar(x,y)
+        axes[axes_num].set_title(data_size)
+        if (axes_num == 0):
+            axes[axes_num].set(xlabel='Number of threads', ylabel='Execution time (microseconds)')
+        else:
+            axes[axes_num].set(xlabel='Number of threads')
+
+        axes_num += 1
+
+    # plt.show()
+    plt.savefig('results/' +  result_name + '.png', dpi=100)
+
+
+if __name__ == "__main__":
+
+    APP_TYPES = ["bodytrack", "facesim", "ferret"]
+    
+    for app in APP_TYPES:
+        generate_plot(app)
---
+++ task3/multithread/parsec/script.sh
@@ -0,0 +1,64 @@
+echo 'Setting up PARSEC adn downloading inputs..'
+
+. parsec-benchmark/env.sh
+. parsec-benchmark/get-inputs
+
+sudo mkdir -p results
+
+
+echo "Building PARSEC apps..."
+parsecmgmt -a build -p bodytrack
+parsecmgmt -a build -p facesim
+parsecmgmt -a build -p ferret
+
+
+echo "Starting benchmarking bodytrack example with PARSEC.."
+
+for i in 1 2 4 8 16 32 64 128
+do
+    echo "Running with $i cores"
+
+    parsecmgmt -a run -p bodytrack -i simsmall -n $i > results/bodytrack_small_"$i".txt
+
+    parsecmgmt -a run -p bodytrack -i simmedium -n $i > results/bodytrack_medium_"$i".txt
+
+    parsecmgmt -a run -p bodytrack -i simlarge -n $i > results/bodytrack_large_"$i".txt
+
+done
+
+echo "Finished bodytrack example."
+
+
+
+echo "Starting benchmarking facesim example with PARSEC.."
+
+for i in 1 2 4 8 16 32 64 128
+do
+    echo "Running with $i cores"
+
+    parsecmgmt -a run -p facesim -i simsmall -n $i > results/facesim_small_"$i".txt
+
+    parsecmgmt -a run -p facesim -i simmedium -n $i > results/facesim_medium_"$i".txt
+
+    parsecmgmt -a run -p facesim -i simlarge -n $i > results/facesim_large_"$i".txt
+
+done
+
+echo "Finished facesim example."
+
+
+echo "Starting benchmarking ferret example with PARSEC.."
+
+for i in 1 2 4 8 16 32 64 128
+do
+    echo "Running with $i cores"
+
+    parsecmgmt -a run -p ferret -i simsmall -n $i > results/ferret_small_"$i".txt
+
+    parsecmgmt -a run -p ferret -i simmedium -n $i > results/ferret_medium_"$i".txt
+
+    parsecmgmt -a run -p ferret -i simlarge -n $i > results/ferret_large_"$i".txt
+
+done
+
+echo "Finished ferret example."
